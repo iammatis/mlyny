d769ef3688d97523802a1413555299d1c899d9e4
Matej Vilk <matej.vilk@gmail.com>
2019-03-30 15:10:41 +0100

    Add wesbos eslint

--- server/config/index.js
+++ server/config/index.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
-  env: process.env.NODE_ENV || "dev",
-  db: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS
-  }
-};
+    env: process.env.NODE_ENV || 'dev',
+    db: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASS
+    }
+}
